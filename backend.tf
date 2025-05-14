683da55fc58c3bb06105a9a9e1ebe248fcc2dbad
Gvrkprasad <glpsk369@gmail.com>
2025-05-14 23:58:33 +0530

    Gameapp-2048 Project files added

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "glps-test-backend-bucket"
-    key            = "terraform_gitactn_eks_game2048/terraform.tfstate"
+    key            = "eksgame2048/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
   }
