ebee1320522a99a6cf64b2fba4f4e26bbd8a4509
GLPSravanaKumar <glps369@gmail.com>
2026-01-12 17:40:55 +0530

    a

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "optum-prod-bkt"
     key            = "terraform.tfstate"
-    region         = "us-east-1"
+    region         = "ap-south-1"
     encrypt        = true
   }
 }
