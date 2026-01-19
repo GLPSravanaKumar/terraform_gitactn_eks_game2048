f21a73bf59c0cf4f12b988ae48f13ddf1e1c052f
GLPSravanaKumar <glps369@gmail.com>
2026-01-19 21:00:56 +0530

    roles cluster,node_group updated

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ resource "aws_vpc" "main" {
   tags = {
     Name = "${var.cluster_name}/vpc"
     "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/ingress.class" = "1"
     "kubernetes.io/cluster/${var.cluster_name}" = "owned"
   }
 }
@@ -23,6 +24,7 @@ resource "aws_subnet" "public" {
     Name                                        = "${var.cluster_name}/public-subnet-${count.index}"
     "kubernetes.io/cluster/${var.cluster_name}" = "owned"
     "kubernetes.io/role/elb"                    = "1"
+    "kubernetes.io/ingress.class" = "1"
   }
 }
 
@@ -66,7 +68,7 @@ resource "aws_route_table_association" "public_rt" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-resource "aws_iam_role" "eks_role" {
+/* resource "aws_iam_role" "eks_role" {
   name = "eks2048role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -86,19 +88,23 @@ resource "aws_iam_role" "eks_role" {
   tags = {
     Name = "${var.cluster_name}/eks-role"
   }
+} */
+
+data "aws_iam_role" "eks_role" {
+  name = "eks2048role"
 }
 
 resource "aws_iam_role_policy_attachment" "EKSClusterPolicy" {
-  role       = aws_iam_role.eks_role.name
+  role       = data.aws_iam_role.eks_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "EKSVPCResourceController" {
-  role       = aws_iam_role.eks_role.name
+  role       = data.aws_iam_role.eks_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
 }
 
-resource "aws_iam_role" "eks_node_role" {
+/* resource "aws_iam_role" "eks_node_role" {
   name = "eksNodeGroupRole"
 
   assume_role_policy = jsonencode({
@@ -114,26 +120,30 @@ resource "aws_iam_role" "eks_node_role" {
   lifecycle {
     prevent_destroy = false
   }
+} */
+
+data "aws_iam_role" "eks_node_role" {
+  name = "eksNodeGroupRole"
 }
 
 resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
-  role       = aws_iam_role.eks_node_role.name
+  role       = data.aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
-  role       = aws_iam_role.eks_node_role.name
+  role       = data.aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
 resource "aws_iam_role_policy_attachment" "ec2_container_registry_readonly" {
-  role       = aws_iam_role.eks_node_role.name
+  role       = data.aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
 resource "aws_eks_cluster" "eks" {
   name     = var.cluster_name
-  role_arn = aws_iam_role.eks_role.arn
+  role_arn = data.aws_iam_role.eks_role.arn
 
   vpc_config {
     subnet_ids = aws_subnet.public[*].id
@@ -149,7 +159,7 @@ resource "aws_eks_cluster" "eks" {
 resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "${var.cluster_name}-node-group"
-  node_role_arn   = aws_iam_role.eks_node_role.arn
+  node_role_arn   = data.aws_iam_role.eks_node_role.arn
   subnet_ids      = aws_subnet.public[*].id
 
   scaling_config {
